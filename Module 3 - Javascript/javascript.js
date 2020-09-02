7245702d480009782a159f8bb1e6e1c8fc7d3fb6
SandervanZaane <58518423+SandervanZaane@users.noreply.github.com>
2020-09-02 12:16:38 +0200

    add 3.1

---
+++ Module 3 - Javascript/javascript.js
@@ -0,0 +1,7 @@
+let name = "Sander";
+let number = (4 + (4 % 3));
+let numberString = ("Kees" + "4");
+console.log(name);
+console.log(number);
+console.log(numberString);
+console.log(typeof(number));
